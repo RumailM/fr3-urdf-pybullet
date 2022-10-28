371710fd9be649dbe7d151190fcdd28bdcf1d121
Rumail Memon <36772370+RumailM@users.noreply.github.com>
2022-10-28 09:25:30 +0400

    Update pybullet-test.py

--- pybullet-test.py
+++ pybullet-test.py
@@ -7,7 +7,7 @@ physcisClient = pybullet.connect(pybullet.GUI)
 pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 planeID = pybullet.loadURDF("plane.urdf")
-robot = pybullet.loadURDF("/franka_description_no_gazebo_specific_tags/robots/fr3/fr3_pybullet.urdf.xml")
+robot = pybullet.loadURDF("/franka_description_pybullet/robots/fr3/fr3_pybullet.urdf.xml")
 
 print("Press enter to quit")
 input()
